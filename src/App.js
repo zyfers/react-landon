26e61e871f1833d7beb5ce347a9b7608ec831598
zyfers <himanshusharma7296@gmail.com>
2022-11-25 07:43:59 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React in less than a day
         </a>
       </header>
     </div>
